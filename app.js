a9e4bf8ad49486ab7e6041aa08ee28205995a7c2
lovesan222 <99589836+lovesan222@users.noreply.github.com>
2022-08-12 12:30:06 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var expressLayouts = require('express-ejs-layouts');
 var logger = require('morgan');
 var path = require('path');
 var routers = require('./routes/route');
 var app = express();
+
+// var expressLayouts = require('express-ejs-layouts');
 
 
 app.set('views', path.join(__dirname, 'views'));
@@ -14,11 +17,13 @@ app.set('layout extractScripts', true);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(expressLayouts);
 app.use(cookieParser());
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// app.use(expressLayouts);
 app.use('/', routers);
 
 module.exports = app;
